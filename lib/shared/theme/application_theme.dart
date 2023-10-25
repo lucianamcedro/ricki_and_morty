c63c0d9fec5f4239706751bb8dd861761b064d93
Luciana Cedro <luciana_cedro@hotmail.com>
2023-10-25 15:39:01 -0300

    criando mais themes

--- lib/shared/theme/application_theme.dart
+++ lib/shared/theme/application_theme.dart
@@ -1,6 +1,7 @@
 import 'package:estudo_rick_xp_flutter/shared/theme/data/app_theme.dart';
 import 'package:estudo_rick_xp_flutter/shared/theme/data/app_theme_model.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 final themes = {
   AppTheme.light: AppThemeModel(
@@ -16,6 +17,44 @@ final themes = {
     themeData: ThemeData(
       brightness: Brightness.dark,
       useMaterial3: true,
+    ),
+  ),
+  AppTheme.halloween: AppThemeModel(
+    theme: AppTheme.halloween,
+    themeData: ThemeData(
+      useMaterial3: true,
+      scaffoldBackgroundColor: Colors.orange[100],
+      textTheme: GoogleFonts.emilysCandyTextTheme(),
+      colorScheme: ColorScheme.fromSwatch(
+        primarySwatch: Colors.orange,
+      ),
+    ),
+  ),
+  AppTheme.piano: AppThemeModel(
+    theme: AppTheme.piano,
+    themeData: ThemeData(
+      brightness: Brightness.dark,
+      useMaterial3: true,
+      scaffoldBackgroundColor: Colors.white,
+      textTheme: GoogleFonts.emilysCandyTextTheme(),
+      cardTheme: const CardTheme(
+        elevation: 1.0,
+        color: Colors.white,
+      ),
+      colorScheme: ColorScheme.fromSwatch(
+        primarySwatch: const MaterialColor(0xFF000000, <int, Color>{
+          50: Color(0xFF000000),
+          100: Color(0xFF000000),
+          200: Color(0xFF000000),
+          300: Color(0xFF000000),
+          400: Color(0xFF000000),
+          500: Color(0xFF000000),
+          600: Color(0xFF000000),
+          700: Color(0xFF000000),
+          800: Color(0xFF000000),
+          900: Color(0xFF000000),
+        }),
+      ),
     ),
   ),
 };
